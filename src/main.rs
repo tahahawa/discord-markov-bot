e3908e8d0dc10827fb970456dfbc261f1c6b4691
Taha Hawa <tahahawa@gmail.com>
2017-02-02 19:47:55 -0500

    use yaml config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate serde_yaml;
+extern crate discord;
+
+use std::fs::File;
+
+use std::io::Read;
+use std::collections::BTreeMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut f = File::open("config.yaml").unwrap();
+    let mut fstr = String::new();
+    let _ = f.read_to_string(&mut fstr);
+
+    let config: BTreeMap<String, String> = serde_yaml::from_str(&mut fstr).unwrap();
+
 }
