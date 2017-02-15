b7350aede1d079a059e9edad11c9c079198dc849
Taha Hawa <tahahawa@gmail.com>
2017-02-15 17:07:25 -0500

    Create table if not exists for SQLite

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #[macro_use] 
 extern crate serenity;
 extern crate serde_yaml;
+extern crate rusqlite;
 
 use std::fs::File;
 use std::io::Read;
 use std::collections::BTreeMap;
 
+
 use serenity::client::Client;
+
+use rusqlite::Connection;
 
 fn main() {
     let mut f = File::open("config.yaml").unwrap();
@@ -14,6 +18,17 @@ fn main() {
     let _ = f.read_to_string(&mut fstr);
 
     let config: BTreeMap<String, String> = serde_yaml::from_str(&mut fstr).unwrap();
+
+    
+    let conn = Connection::open(config.get("db").unwrap() ).unwrap();
+
+        conn.execute("CREATE TABLE IF NOT EXISTS messages (
+                  id              INTEGER PRIMARY KEY,
+                  author            INTEGER NOT NULL,
+                  content    TEXT NOT NULL,
+                  timestamp            TEXT NOT NULL
+                  )", &[]).unwrap();
+
 
     let mut client = Client::login_bot(&config.get("token").expect("token"));
     client.with_framework(|f| f
