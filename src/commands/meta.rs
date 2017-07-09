6c197d4d835d33ef1ba06ff903c2a1309aedc79b
Taha Hawa <tahahawa@gmail.com>
2017-07-09 21:23:46 +0000

    formatting and better feedback

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -1,8 +1,7 @@
 use serenity::client::*;
 use serenity::model::*;
 
-pub fn ping(_context: &mut Context,
-               message: &Message, _args: Vec<String>)-> Result<(), String>  {
+pub fn ping(_context: &mut Context, message: &Message, _args: Vec<String>) -> Result<(), String> {
     let _ = message.reply("Pong!");
     Ok(())
 }
