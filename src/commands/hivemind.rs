f174a9f60e9cef8ff7492fcd0a2957778a18b556
Taha Hawa <tahahawa@gmail.com>
2017-12-21 20:56:51 -0500

    parse usize and usernames in a better, less dumb way

--- src/commands/hivemind.rs
+++ src/commands/hivemind.rs
@@ -5,79 +5,44 @@ use regex::Regex;
 use markov::Chain;
 use Sqlpool;
 
-pub fn hivemind(_context: &mut Context, message: &Message, args: Args) -> Result<(), CommandError> {
+pub fn hivemind(_context: &mut Context, message: &Message, mut args: Args) -> Result<(), CommandError> {
     let _ = message.channel_id.broadcast_typing();
 
     let re = Regex::new(r"(<@!?\d*>)").unwrap();
 
-    let _args: Vec<String> = args.multiple().unwrap();
+    println!("args: {:?}", args);
+    
+    let count: usize = args.single().unwrap_or(1);
+
 
     let data = _context.data.lock();
     let pool = data.get::<Sqlpool>().unwrap().clone();
     let conn = pool.get().unwrap();
     drop(data);
 
-    if !_args.is_empty() {
-        let mut chain: Chain<String> = Chain::new();
+    let mut chain: Chain<String> = Chain::new();
 
-        let mut stmt = conn.prepare("SELECT * FROM messages where content not like '%~hivemind%' and content not like '%~impersonate%' and content not like '%~ping%' ").unwrap();
-        let rows = stmt.query_map_named(&[], |row| row.get(3)).unwrap();
+    let mut stmt = conn.prepare("SELECT * FROM messages where content not like '%~hivemind%' and content not like '%~impersonate%' and content not like '%~ping%' ").unwrap();
+    let rows = stmt.query_map_named(&[], |row| row.get(3)).unwrap();
 
-        let mut messages = Vec::<String>::new();
-        for content in rows {
-            messages.push(content.unwrap());
+    let mut messages = Vec::<String>::new();
+    for content in rows {
+        messages.push(content.unwrap());
+    }
+
+    if !messages.is_empty() {
+        for m in messages {
+            chain.feed_str(&m);
         }
 
-        if !messages.is_empty() {
-            for m in messages {
-                chain.feed_str(&m);
-            }
-
-            let re_iter = Regex::new(r"\D").unwrap();
-            let iter_test = re_iter.replace_all(&_args[0], "");
-
-            let iter: usize = if !iter_test.is_empty() {
-                if iter_test.parse::<usize>().is_ok() {
-                    iter_test.parse::<usize>().unwrap()
-                } else {
-                    1
-                }
-            } else {
-                1
-            };
-
-            for line in chain.str_iter_for(iter) {
-                let _ = message.channel_id.say(&re.replace_all(&line, "@mention")
-                    .into_owned());
-                //println!("{}", line);
-                let _ = message.channel_id.broadcast_typing();
-            }
-        } else {
-            let _ = message.reply("They haven't said anything");
+        for line in chain.str_iter_for(count) {
+            let _ = message.channel_id.say(&re.replace_all(&line, "@mention")
+                .into_owned());
+            //println!("{}", line);
+            let _ = message.channel_id.broadcast_typing();
         }
     } else {
-        let mut chain: Chain<String> = Chain::new();
-
-        let mut stmt = conn.prepare("SELECT * FROM messages where content not like '%~hivemind%' and content not like '%~impersonate%' and content not like '%~ping%' ").unwrap();
-        let rows = stmt.query_map_named(&[], |row| row.get(3)).unwrap();
-
-        let mut messages = Vec::<String>::new();
-        for content in rows {
-            messages.push(content.unwrap());
-        }
-        let _ = message.channel_id.broadcast_typing();
-
-        if !messages.is_empty() {
-            for m in messages {
-                chain.feed_str(&m);
-            }
-            let _ = message.channel_id.say(&re.replace_all(
-                &chain.generate_str(),
-                "@mention",
-            ).into_owned());
-        } else {
-            let _ = message.reply("They haven't said anything");
-        }
+        let _ = message.reply("They haven't said anything");
     }
     Ok(())
 }
--- src/commands/impersonate.rs
+++ src/commands/impersonate.rs
@@ -4,23 +4,25 @@ use serenity::framework::standard::*;
 use regex::Regex;
 use markov::Chain;
 
-// use commands;
-
 use Sqlpool;
 
-pub fn impersonate(_context: &mut Context, message: &Message, args: Args) -> Result<(), CommandError> {
+pub fn impersonate(_context: &mut Context, message: &Message, mut args: Args) -> Result<(), CommandError> {
 
-    let _args: Vec<String> = args.multiple_quoted().unwrap();
+    println!("args: {:?}", args);
+    
+    let username: String = args.single_quoted().unwrap();
+    let count: usize = args.single().unwrap_or(1);
+
     // let chan = message.channel_id.get().unwrap();
 
-    let _ = message.channel_id.broadcast_typing();;
+    let _ = message.channel_id.broadcast_typing();
 
     let re = Regex::new(r"(<@!?\d*>)").unwrap();
 
     let guild_arc = message.guild().unwrap();
     let guild = guild_arc.read();
 
-    let member = guild.member_named( &_args[0] );
+    let member = guild.member_named( &username );
 
     let mut user = None;
 
@@ -34,7 +36,7 @@ pub fn impersonate(_context: &mut Context, message: &Message, args: Args) -> Res
     let conn = pool.get().unwrap();
     drop(data);
 
-    if user.is_some() && _args.len() > 1 {
+    if user.is_some() {
         let user = user.unwrap();
         let mut chain: Chain<String> = Chain::new();
 
@@ -52,13 +54,13 @@ pub fn impersonate(_context: &mut Context, message: &Message, args: Args) -> Res
                 chain.feed_str(&m);
             }
 
-            let re_iter = Regex::new(r"\D").unwrap();
-            let iter_test = re_iter.replace_all(&_args[1], "");
+            // let re_iter = Regex::new(r"\D").unwrap();
+            // let iter_test = re_iter.replace_all(&count, "");
 
 
-            let iter: usize = iter_test.parse::<usize>().unwrap_or(1);
+            // let iter: usize = iter_test.parse::<usize>().unwrap_or(1);
 
-            for line in chain.str_iter_for(iter) {
+            for line in chain.str_iter_for(count) {
                 let _ = message.channel_id.say(&re.replace_all(&line, "@mention")
                     .into_owned());
                 //println!("{}", line);
@@ -67,31 +69,31 @@ pub fn impersonate(_context: &mut Context, message: &Message, args: Args) -> Res
         } else {
             let _ = message.reply("They haven't said anything");
         }
-    } else if user.is_some() {
-        let user = user.unwrap();
-        let mut chain: Chain<String> = Chain::new();
+    // } else if user.is_some() {
+    //     let user = user.unwrap();
+    //     let mut chain: Chain<String> = Chain::new();
 
-        let mut stmt = conn.prepare("SELECT * FROM messages where author = :id and content not like '%~hivemind%' and content not like '%~impersonate%' and content not like '%~ping%' ").unwrap();
-        let rows = stmt.query_map_named(&[(":id", &(user.id.0.to_string()))], |row| row.get(3))
-            .unwrap();
+    //     let mut stmt = conn.prepare("SELECT * FROM messages where author = :id and content not like '%~hivemind%' and content not like '%~impersonate%' and content not like '%~ping%' ").unwrap();
+    //     let rows = stmt.query_map_named(&[(":id", &(user.id.0.to_string()))], |row| row.get(3))
+    //         .unwrap();
 
-        let mut messages = Vec::<String>::new();
-        for content in rows {
-            messages.push(content.unwrap());
-        }
-        let _ = message.channel_id.broadcast_typing();
+    //     let mut messages = Vec::<String>::new();
+    //     for content in rows {
+    //         messages.push(content.unwrap());
+    //     }
+    //     let _ = message.channel_id.broadcast_typing();
 
-        if !messages.is_empty() {
-            for m in messages {
-                chain.feed_str(&m);
-            }
-            let _ = message.channel_id.say(&re.replace_all(
-                &chain.generate_str(),
-                "@mention",
-            ).into_owned());
-        } else {
-            let _ = message.reply("They haven't said anything");
-        }
+    //     if !messages.is_empty() {
+    //         for m in messages {
+    //             chain.feed_str(&m);
+    //         }
+    //         let _ = message.channel_id.say(&re.replace_all(
+    //             &chain.generate_str(),
+    //             "@mention",
+    //         ).into_owned());
+    //     } else {
+    //         let _ = message.reply("They haven't said anything");
+    //     }
     } else {
         let _ = message.reply("No user found");
     }
